ea22ee201217a51201733901aa197118cc590065
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-08-15 22:08:19 +0200

    feat: add info dialog

--- web/src/lib/index.ts
+++ web/src/lib/index.ts
@@ -3,6 +3,8 @@ export { default as CooldownNotice } from './components/CooldownNotice.svelte';
 export { default as ReasonPrompt } from './components/ReasonPrompt.svelte';
 export { default as NavToolBar } from './components/NavToolBar.svelte';
 export { default as HistoryTile } from './components/HistoryTile.svelte';
+export { default as InfoDialog } from './components/InfoDialog.svelte';
+
 export { restApi } from './api/rest';
 export { wsClient } from './api/ws.svelte';
 export { bulbState } from './state/bulb.svelte';
